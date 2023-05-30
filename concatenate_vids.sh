9a487a38426383c8a42fb944800453bed885ef57
Anton Shyshko <pozernishku@gmail.com>
2023-05-30 19:44:53 +0300

    docs: add TODOs and FIXME

--- concatenate_vids.sh
+++ concatenate_vids.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# FIXME: Muted output. Investigate and fix the case when there is a video without sound
 ffmpeg -hide_banner -f concat -i file_list.txt -c copy video_"$(date +%Y%m%d_%H%M%S)".mp4
 rm -rf blured_vids
 rm file_list.txt
--- download_and_prepare.sh
+++ download_and_prepare.sh
@@ -2,11 +2,13 @@
 
 url="https://www.reddit.com/r/TikTokCringe/hot.json?limit=2"
 user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0"
+# TODO: Get rid of not items that are not videos
 youtube-dl $(curl -s -H "User-Agent: $user_agent" "$url" | jq '.' | grep url_overridden_by_dest | grep -Eoh "https:\/\/v\.redd\.it\/\w{13}")
 
 mkdir blured_vids
 for f in *.mp4;
 do
+  # TODO: Try to do it using multiprocessing
   ffmpeg -hide_banner -i "$f" -lavfi '[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16' -vb 800K blured_vids/"$f";
 done
 
