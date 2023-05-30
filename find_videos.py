d80043c0c8f98b729411e6845ff6cfa049044b99
Anton Shyshko <pozernishku@gmail.com>
2023-05-30 22:31:50 +0300

    feat: update find_videos.py -> output urls line by line

--- find_videos.py
+++ find_videos.py
@@ -1,9 +1,10 @@
-from argparse import ArgumentParser
 import json
+from argparse import ArgumentParser
+
 import jmespath
 
 
-def get_videos(json_obj: dict | list | None) -> list[str]:
+def get_videos(json_obj: dict | list | None) -> list[str] | None:
     # TODO: Get also cross-post video items
     jpath = "data.children[?data.is_video == `true`].data.url_overridden_by_dest"
     urls_list = jmespath.search(jpath, json_obj)
@@ -12,9 +13,12 @@ def get_videos(json_obj: dict | list | None) -> list[str]:
 
 if __name__ == "__main__":
     p = ArgumentParser()
-    # breakpoint()
     default_json = "{}"
     p.add_argument("--json", default=default_json, required=False, type=str)
     namespace = p.parse_known_args()[0]
     json_data = json.loads(namespace.json)
-    print(get_videos(json_data))
+    videos = get_videos(json_data)
+    if not videos:
+        raise Exception("No videos found")
+    for v in videos:
+        print(v)
