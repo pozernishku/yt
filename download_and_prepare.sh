2abb193dd188ce359f48b9a3e9114b6b012b7b34
Anton Shyshko <pozernishku@gmail.com>
2023-06-03 22:50:32 +0300

    docs: update download_and_prepare.sh -> add one more TODO

--- download_and_prepare.sh
+++ download_and_prepare.sh
@@ -7,7 +7,8 @@ python -m download --url "$url"
 mkdir blured_vids
 for f in *.mp4;
 do
-  # TODO: Try to do it using multiprocessing
+  # TODO: Try to do it using multiprocessing (looks like it's already multiprocessing)
+  # TODO: Update the command below. It should work on video without audio stream as well
   ffmpeg -hide_banner -i "$f" -lavfi '[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16' -vb 800K blured_vids/"$f";
 done
 
